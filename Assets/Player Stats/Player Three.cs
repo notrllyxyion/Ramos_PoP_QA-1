4629b3a8e2caa29f26a6749f56216fbf59b91994
notrllyxyion <mikelreuelramos69@gmail.com>
2024-09-24 22:01:53 +0800

    finishing touches
    yes

--- Assets/Player Stats/Player Three.cs
+++ Assets/Player Stats/Player Three.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerThree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI text_w;
+
+    public string Name = "Jah-Rek Romeus Gayod panot";
+    public int Age = 25;
+    public int Health = 1500;
+    public float Aura = -999999999.9999f;
+    public int rizz = -13130000;
+    public int armor = -9999;
+    public int Speed =-44000;
+    public int mana = 313100;
+    public int PhysicalDamage = 9009;
+    public int MagicalDamage = 3003;
+
+
+    public void ButtonPressed()
     {
-        
+        text_w.text = $" Player One Stats: \r\n \r\n Name: {Name} \r\n Age: {25} \r\n Health: {Health} \r\n Aura: {Aura} \r\n Rizz: {rizz} \r\n Armor: {armor} \r\n Speed: {Speed} \r\n Mana: {mana} \r\n Physical Damage: {PhysicalDamage} \r\n Magical Damage; {MagicalDamage} ";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Player Stats/Player Two.cs
+++ Assets/Player Stats/Player Two.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerTwo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI text_w;
+
+    public string Name = "Arjan Manzano pogi";
+    public int Age = 25;
+    public int Health = 1500;
+    public float Aura = 93199.9999f;
+    public int rizz = 1220;
+    public int armor = 31319;
+    public int Speed = 50000;
+    public int mana = 131310;
+    public int PhysicalDamage = 9131;
+    public int MagicalDamage = 301313;
+
+
+    public void ButtonPressed()
     {
-        
+        text_w.text = $" Player One Stats: \r\n \r\n Name: {Name} \r\n Age: {25} \r\n Health: {Health} \r\n Aura: {Aura} \r\n Rizz: {rizz} \r\n Armor: {armor} \r\n Speed: {Speed} \r\n Mana: {mana} \r\n Physical Damage: {PhysicalDamage} \r\n Magical Damage; {MagicalDamage} ";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
